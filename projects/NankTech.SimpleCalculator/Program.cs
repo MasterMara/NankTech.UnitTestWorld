6ffb0f7961b72dbe17687ffb2aed77fc3e93b7b4
mukaracabey <karacabey.mustafa@hepsiburada.com>
2022-09-22 15:00:16 +0300

    [Added Code Coverage]

--- projects/NankTech.SimpleCalculator/Program.cs
+++ projects/NankTech.SimpleCalculator/Program.cs
@@ -3,3 +3,4 @@
 Console.WriteLine("I Have a simple Calculator");
 
 //dotnet new xunit -o NankTech.SimpleLibrary.API.Tests
+// dotnet new sln -n XUnit.Coverage -o NankTech.SimpleLibrary.API.CodeCoverage   For Code Coverage
